d1776b536d5bbf6e76bbb461401ff6d524bebf09
delighted <deborahdelighted5@gmail.com>
2024-05-23 14:04:40 +0100

    edit user field

--- src/main/java/com/ubuntucontinues/ubuntu/data/models/User.java
+++ src/main/java/com/ubuntucontinues/ubuntu/data/models/User.java
@@ -4,6 +4,7 @@ import com.ubuntucontinues.ubuntu.data.enums.Status;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
@@ -14,7 +15,8 @@ public class User {
     private String userName;
     private String fullName;
     private Status status;
-    private String cohortNumber;
+    @DBRef
+    private Cohort cohort;
     private String email;
     private String profilePicture;
 }
