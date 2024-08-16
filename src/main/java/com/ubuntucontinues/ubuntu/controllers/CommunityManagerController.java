74a41e2549067845c68840de92907edca911461a
delighted <deborahdelighted5@gmail.com>
2024-08-16 13:52:57 +0100

    added try and catch to CommunityManagerController,QuestionController,ReplyController

--- src/main/java/com/ubuntucontinues/ubuntu/controllers/CommunityManagerController.java
+++ src/main/java/com/ubuntucontinues/ubuntu/controllers/CommunityManagerController.java
@@ -3,10 +3,8 @@ package com.ubuntucontinues.ubuntu.controllers;
 import com.ubuntucontinues.ubuntu.dto.requests.AddStudentRequest;
 import com.ubuntucontinues.ubuntu.dto.requests.CreateEventRequest;
 import com.ubuntucontinues.ubuntu.dto.requests.UpdateEventRequest;
-import com.ubuntucontinues.ubuntu.exceptions.EventAlreadyExistException;
-import com.ubuntucontinues.ubuntu.exceptions.EventDoesntExistException;
-import com.ubuntucontinues.ubuntu.exceptions.EventExistException;
-import com.ubuntucontinues.ubuntu.exceptions.UserExistException;
+import com.ubuntucontinues.ubuntu.dto.responses.ApiResponse;
+import com.ubuntucontinues.ubuntu.exceptions.*;
 import com.ubuntucontinues.ubuntu.services.CommunityManagerService;
 import com.ubuntucontinues.ubuntu.services.EventService;
 import com.ubuntucontinues.ubuntu.services.UserService;
@@ -28,38 +26,71 @@ public class CommunityManagerController {
     private final UserService userService;
 
     @PostMapping("/createEvent")
-    public ResponseEntity<?> createEvent(@RequestPart(value = "image" ,required = false) MultipartFile file, @ModelAttribute CreateEventRequest createEventRequest) throws EventAlreadyExistException, IOException {
-        return new ResponseEntity<>(eventServices.createEvent(createEventRequest,file), HttpStatus.OK);
+    public ResponseEntity<ApiResponse<?>> createEvent(@RequestPart(value = "image", required = false) MultipartFile file, @ModelAttribute CreateEventRequest createEventRequest) {
+        try {
+            return new ResponseEntity<>(new ApiResponse<>(true, eventServices.createEvent(createEventRequest, file)), HttpStatus.OK);
+        } catch (EventAlreadyExistException | IOException exception) {
+            return ResponseEntity.badRequest().body(new ApiResponse<>(false, exception.getMessage()));
+        }
     }
+
     @GetMapping("/findEvent")
-    public ResponseEntity<?> findEvent(@RequestParam("eventId") String eventId) throws EventDoesntExistException {
-        return new ResponseEntity<>(eventServices.findEvent(eventId), HttpStatus.FOUND);
+    public ResponseEntity<ApiResponse<?>> findEvent(@RequestParam("eventId") String eventId) {
+        try {
+            return new ResponseEntity<>(new ApiResponse<>(true, eventServices.findEvent(eventId)), HttpStatus.FOUND);
+
+        } catch (EventDoesntExistException exception) {
+            return ResponseEntity.badRequest().body(new ApiResponse<>(false, exception.getMessage()));
+        }
     }
 
     @GetMapping("/findAllEvent")
-    public ResponseEntity<?> findAllEvent() throws EventDoesntExistException {
-        return new ResponseEntity<>(eventServices.findAllEvent(), HttpStatus.OK);
+    public ResponseEntity<ApiResponse<?>> findAllEvent() throws EventDoesntExistException {
+        try {
+            return new ResponseEntity<>(new ApiResponse<>(true, eventServices.findAllEvent()), HttpStatus.OK);
+        } catch (EventDoesntExistException exception) {
+            return ResponseEntity.badRequest().body(new ApiResponse<>(false, exception.getMessage()));
+        }
     }
 
     @PostMapping("/deleteEvent")
-    public ResponseEntity<?> deleteEvent(@RequestParam("eventId") String eventId) throws EventDoesntExistException {
-        return new ResponseEntity<>(eventServices.deleteEvent(eventId), HttpStatus.OK);
+    public ResponseEntity<ApiResponse<?>> deleteEvent(@RequestParam("eventId") String eventId) throws EventDoesntExistException {
+        try {
+            return new ResponseEntity<>(new ApiResponse<>(true, eventServices.deleteEvent(eventId)), HttpStatus.OK);
+
+        } catch (EventDoesntExistException exception) {
+            return ResponseEntity.badRequest().body(new ApiResponse<>(false, exception.getMessage()));
+        }
+
     }
 
     @PostMapping("/add_student")
-    public ResponseEntity<?> addStudent(@RequestBody AddStudentRequest request) throws UserExistException {
-        return new ResponseEntity<>(managerService.addStudent(request), HttpStatus.ACCEPTED);
+    public ResponseEntity<ApiResponse<?>> addStudent(@RequestBody AddStudentRequest request) {
+        try {
+            return new ResponseEntity<>(new ApiResponse<>(true, managerService.addStudent(request)), HttpStatus.ACCEPTED);
+        } catch (UserExistException exception) {
+            return ResponseEntity.badRequest().body(new ApiResponse<>(false, exception.getMessage()));
+        }
     }
 
     @PostMapping("/event")
-    public ResponseEntity<?> updateEvent(@RequestBody UpdateEventRequest request) throws EventExistException {
-        return new ResponseEntity<>(managerService.updateEvent(request), HttpStatus.CREATED);
+    public ResponseEntity<ApiResponse<?>> updateEvent(@RequestBody UpdateEventRequest request) {
+        try {
+            return new ResponseEntity<>(new ApiResponse<>(true, managerService.updateEvent(request)), HttpStatus.CREATED);
+        } catch (EventExistException exception) {
+            return ResponseEntity.badRequest().body(new ApiResponse<>(false, exception.getMessage()));
+        }
     }
 
 
     @DeleteMapping("/remove_student")
-    public ResponseEntity<?> removeStudent(@RequestBody String userId) throws  UserExistException {
-        return new ResponseEntity<>(userService.dropDown(userId), HttpStatus.OK);
+    public ResponseEntity<ApiResponse<?>> removeStudent(@RequestBody String userId) {
+        try {
+            return new ResponseEntity<>(new ApiResponse<>(true, userService.dropDown(userId)), HttpStatus.OK);
+
+        } catch (UserExistException exception) {
+            return ResponseEntity.badRequest().body(new ApiResponse<>(false, exception.getMessage()));
+        }
     }
 
 
